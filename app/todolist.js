30c93e302e2a9cbd7278302b76cfde05a89d1920
lucianaDilizio <lgarcia@makingsense.com>
2017-03-27 15:01:47 -0300

    Improve again the return early method. Also, delete the item object in the Controller, it's only uses the description

--- app/todolist.js
+++ app/todolist.js
@@ -2,12 +2,15 @@
   var app = angular.module('toDoList', []);
 
   app.controller('ItemController', ['itemsApi', function(itemsApi) {
+    this.itemDescription = "";
     this.items = itemsApi.items;
-    this.item = {};
 
     this.addItem = function(itemDescription) {
-      this.item = {};
-      if(itemDescription) return itemsApi.addItem(itemDescription);
+      if(!itemDescription)
+        return;
+
+      itemsApi.addItem(itemDescription);
+      this.itemDescription = "";
     };
 
     this.deleteItem = function(item) {
